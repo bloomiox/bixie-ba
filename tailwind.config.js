46006c996e17b7572f16139e7527f2855ce91b8d
BIXIE <hello@bixie.ba>
2026-08-17 19:23:21 +0000

    fix: Tailwind CSS build za blog postove (utility klase) + .tags

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './blog/**/*.html',
+    './en/**/*.html',
+    './de/**/*.html',
+    './index.html',
+    './**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
